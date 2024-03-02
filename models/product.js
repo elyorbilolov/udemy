e2290baf6b8e84eefac09a2d73bcaee0146fda13
Elyor bilolov <elyorbilolovelyorbilolov@gmail.com>
2024-03-02 22:29:27 +0500

    02.03.2024

--- models/product.js
+++ models/product.js
@@ -2,19 +2,20 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
-      // update the product
+      // Update the product
       dbOp = db
         .collection("products")
         .updateOne({ _id: this._id }, { $set: this });
@@ -40,7 +41,9 @@ class Product {
         console.log(products);
         return products;
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   static findById(prodId) {
